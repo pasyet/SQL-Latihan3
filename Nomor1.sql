e4f4388ab12a8b8f6ece05f17a26a4c0d86956f0
pasyet <farapasyetnabila@gmail.com>
2018-01-13 05:28:25 +0700

    Latihan 3

---
+++ Nomor1.sql
@@ -0,0 +1,2 @@
+INSERT INTO countries (country_id, country_name, region_id)
+    VALUES('ID','Indonesia','3');
---
+++ Nomor2.sql
@@ -0,0 +1,6 @@
+INSERT INTO locations (location_id, street_address, postal_code, city, state_province, country_id)
+    VALUES(6232,'Cinunuk','40526','Kab.Bandung', 'Jawa Barat', 'ID'),
+      (6231,'Ujung Berung','40521','Kab.Bandung', 'Jawa Barat', 'ID'),
+      (6233,'Margahayu Raya','40525','Kab.Bandung', 'Jawa Barat', 'ID'),
+      (6230,'Blok M','40520','Jakarta Selatan', 'DKI Jakarta', 'ID'),
+      (6220,'Slipi','40521','Jakarta Utara', 'DKI Jakarta', 'ID');
